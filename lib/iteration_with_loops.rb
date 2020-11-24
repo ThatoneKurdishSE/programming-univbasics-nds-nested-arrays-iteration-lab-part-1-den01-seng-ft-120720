84edf1e626019522693e7607ffbb4352cbc20950
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-24 23:54:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,7 @@
 def find_even_values(src)
-[[10, 11], [99, 50, 3, 4], [23, 41]]
+array_of_arrays=[
+[10, 11], [99, 50, 3, 4], [23, 41]
+  ]
+  if array_of_arrays[10][11].even?
+    p array_of_arrays[][]
 end
