420ea8e684c2d35c50b14bd6585c8689038c9054
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-25 00:00:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,6 +5,8 @@ while row_index<src.count do
   while element_index<src[row_index].count do
     if src[row_index]
       [element_index].even?
+      p src[row_index]
+      [element_index]
     end
     element_index+=1
   end
